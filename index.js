dcb72b3cc1ad35ef236682ec8758ba3159f4df7f
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-06-14 00:02:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
-  if (username === 'admin' && password === 'password') {
+  if (username === 'gdps' && password === 'ccproject10') {
     req.session.username = username;
     res.redirect('/dashboard');
   } else {
